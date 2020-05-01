5d0ba783a30836de64e1ec929bb2ee7374c02bd7
haidarajaafar <63954469+haidarajaafar@users.noreply.github.com>
2020-05-01 02:15:13 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {telegraf_token:'1240900504:AAEARUVPOXmPxhfLdeohCrQZp9XYyV773AI',
+database:'mongodb+srv://haidara:abujaafar420haidara420@@--##@cluster0-r9zio.mongodb.net/test?retryWrites=true&w=majority',
+secret:'abujaafar420haidara420@@--##'};
+
